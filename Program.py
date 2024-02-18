e58e9b62b275fd78151e463629b71a7cc09f9665
AceBurgundy <samadriansabalo99@gmail.com>
2024-02-19 00:11:21 +0800

    added icons, title

--- Program.py
+++ Program.py
@@ -1,11 +1,10 @@
-from typing import List, Callable
 import customtkinter
 
 from Navigation import Navigation
 from Canvas import OpenGLCanvas
 from Shape import Shape
 
-from constants import WINDOW_SIZE
+from constants import *
 
 class App(customtkinter.CTk):
     def __init__(self) -> None:
@@ -14,16 +13,16 @@ class App(customtkinter.CTk):
         """
         super().__init__()
         self.geometry(WINDOW_SIZE)
+        self.title("2D Shape Drawer by: Sam Adrian P. Sabalo")
+        self.iconbitmap(ICON_PATH)
 
         self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=5)
+        self.grid_columnconfigure(0, weight=0, uniform="nav_col")
+        self.grid_columnconfigure(1, weight=1, uniform="nav_col")
 
-        buttons: List[Callable] = Shape.all_shapes()
-
-        left_content: Navigation = Navigation(parent=self, buttons=buttons)
+        left_content: Navigation = Navigation(parent=self)
         left_content.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
+        left_content.insert_buttons(Shape.all_shapes())
 
         self.right_content: OpenGLCanvas = OpenGLCanvas(self)
         self.right_content.grid(row=0, column=1, padx=(0, 5), pady=5, sticky="nsew")
-
