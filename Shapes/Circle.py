5c8d5d5a9587eee2667a1f9cc23114453706b0e3
AceBurgundy <samadriansabalo99@gmail.com>
2024-02-24 16:50:07 +0800

    added within bounds docstring and moved simplified polygon drawing inside Shape.

--- Shapes/Circle.py
+++ Shapes/Circle.py
@@ -1,9 +1,9 @@
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
-from Shapes import Shape
 from OpenGL.GL import *
 from math import *
 
+from Shapes.Shape import Shape
 from custom_types import *
 from constants import *
 
@@ -11,35 +11,25 @@ class Circle(Shape):
     """
     Represents a circle shape.
     """
+    def within_bounds(self, mouse_x: int, mouse_y: int) -> bool:
+        """
+        Checks if the given coordinates are within the bounds of the shape.
+
+        Args:
+            mouse_x (int): The x-coordinate of the mouse cursor.
+            mouse_y (int): The y-coordinate of the mouse cursor.
+
+        Returns:
+            bool: True if the coordinates are within the bounds of the shape, False otherwise.
+        """
+        # Calculate the distance between the point and the center using the distance formula
+        distance = sqrt((mouse_x - self.center_x) ** 2 + (mouse_y - self.center_y) ** 2)
+
+        # Check if the distance is less than or equal to the radius
+        return distance <= self.half_size
 
     def draw(self) -> None:
         """
         Draws the circle shape.
         """
-        center_x = (self.start_coordinates[0] + self.end_coordinates[0]) / 2
-        center_y = (self.start_coordinates[1] + self.end_coordinates[1]) / 2
-        half_size = min(self.width, self.height) / 2
-
-        # Draw border lines if selected
-        if self.selected:
-            border_offset = 10
-            glColor3f(*self.border_color)
-
-            glBegin(GL_LINE_LOOP)
-            for index in range(100):
-                angle = 2 * pi * index / 100
-                x = center_x + (half_size + border_offset) * cos(angle)
-                y = center_y + (half_size + border_offset) * sin(angle)
-                glVertex2f(x, y)
-            glEnd()
-
-        glColor3f(*self.background_color)
-        glBegin(GL_POLYGON)
-        for index in range(100):
-            angle = 2 * pi * index / 100
-            x = center_x + half_size * cos(angle)
-            y = center_y + half_size * sin(angle)
-            glVertex2f(x, y)
-        glEnd()
-
-        glFlush()
+        self.draw_polygon(100)
--- Shapes/Octagon.py
+++ Shapes/Octagon.py
@@ -1,9 +1,9 @@
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
-from Shapes import Shape
 from OpenGL.GL import *
 from math import *
 
+from Shapes.Shape import Shape
 from custom_types import *
 from constants import *
 
@@ -11,34 +11,22 @@ class Octagon(Shape):
     """
     Represents an octagon shape.
     """
+    def within_bounds(self, mouse_x: int, mouse_y: int) -> bool:
+        """
+        Checks if the given coordinates are within the bounds of the shape.
+
+        Args:
+            mouse_x (int): The x-coordinate of the mouse cursor.
+            mouse_y (int): The y-coordinate of the mouse cursor.
+
+        Returns:
+            bool: True if the coordinates are within the bounds of the shape, False otherwise.
+        """
+        return super().use_within_polygon_bounds(mouse_x, mouse_y, 8)
 
     def draw(self) -> None:
         """
-        Draws the octagon shape.
+        Draws the hexagon shape.
         """
-        center_x = (self.start_coordinates[0] + self.end_coordinates[0]) / 2
-        center_y = (self.start_coordinates[1] + self.end_coordinates[1]) / 2
-        half_size: NUMBER = min(self.width, self.height) / 2
+        self.draw_polygon(8)
 
-        if self.selected:
-            border_offset = 10
-            glColor3f(*self.border_color)
-
-            glBegin(GL_LINE_LOOP)
-            for index in range(8):
-                angle = 2 * pi * index / 8
-                x: NUMBER = center_x + (half_size + border_offset) * cos(angle)
-                y: NUMBER = center_y + (half_size + border_offset) * sin(angle)
-                glVertex2f(x, y)
-            glEnd()
-
-        glColor3f(*self.background_color)
-        glBegin(GL_POLYGON)
-        for index in range(8):
-            angle = 2 * pi * index / 8
-            x: NUMBER = center_x + half_size * cos(angle)
-            y: NUMBER = center_y + half_size * sin(angle)
-            glVertex2f(x, y)
-        glEnd()
-
-        glFlush()
