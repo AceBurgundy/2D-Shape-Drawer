c0402f415561063b9c0d896accefeea92855de6e
AceBurgundy <samadriansabalo99@gmail.com>
2024-02-24 16:58:35 +0800

    Added key press events, and extended mouse click capabilities.

--- Canvas.py
+++ Canvas.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+
 if TYPE_CHECKING:
     from Program import App
 
@@ -15,8 +16,10 @@ import pyopengltk
 
 from KeyPress import get_pressed_status
 from custom_types import COORDINATE
+from AppManager import AppManager
+from Shapes.Manager import shapes
 from Shapes.Shape import Shape
-from Manager import Manager
+from CTkToast import CTkToast
 
 class OpenGLCanvas(pyopengltk.OpenGLFrame):
     def __init__(self, parent: App, **kwargs) -> None:
@@ -58,9 +61,6 @@ class OpenGLCanvas(pyopengltk.OpenGLFrame):
         """
         Handles key pressed events sent from main CTk frame
         """
-        if Manager.selected_shape == None:
-            return
-
         press_status: Dict[str, List|str] = get_pressed_status(event)
         state: List[str] = press_status.get('state', None)
         key: str = press_status['key']
@@ -68,68 +68,142 @@ class OpenGLCanvas(pyopengltk.OpenGLFrame):
         if not state:
             return
 
-        if len(state) > 2:
-            return
-
         pressed_shift: bool = 'Shift' in state
         pressed_control: bool = 'Control' in state
         held_both: bool = pressed_shift and pressed_control
 
-        pressed_plus: bool = held_both and key == 'plus'
-        pressed_minus: bool = held_both and key == 'underscore'
+        if held_both:
 
-        if pressed_plus:
-            print("increase shape")
-            return
+            if key == 'plus':
+                if AppManager.selected_shape == None:
+                    CTkToast.toast("Select a shape first to increase its size")
+                    return
 
-        if pressed_minus:
-            print("decrease_shape")
-            return
+                AppManager.selected_shape.increase_shape()
+                return
+
+            if key == 'underscore':
+                if AppManager.selected_shape == None:
+                    CTkToast.toast("Select a shape first to decrease its size")
+                    return
+
+                AppManager.selected_shape.decrease_shape()
+                return
+
+            if key == 'Left':
+                if AppManager.selected_shape == None:
+                    CTkToast.toast("Select a shape first to rotate it to counter clockwise")
+                    return
+
+                AppManager.selected_shape.rotate_left()
+                return
+
+            if key == 'Right':
+                if AppManager.selected_shape == None:
+                    CTkToast.toast("Select a shape first to rotate it clockwise")
+                    return
+
+                AppManager.selected_shape.rotate_right()
+                return
+
+        else:
+
+            if key == 'Up':
+                if AppManager.selected_shape == None:
+                    CTkToast.toast("Select a shape first to move it up")
+                    return
+
+                AppManager.selected_shape.move_up()
+                return
+
+            if key == 'Down':
+                if AppManager.selected_shape == None:
+                    CTkToast.toast("Select a shape first to move it down")
+                    return
+
+                AppManager.selected_shape.move_down()
+                return
+
+            if key == 'Left':
+                if AppManager.selected_shape == None:
+                    CTkToast.toast("Select a shape first to move it to the left")
+                    return
+
+                AppManager.selected_shape.move_left()
+                return
+
+            if key == 'Right':
+                if AppManager.selected_shape == None:
+                    CTkToast.toast("Select a shape first to move it to the right")
+                    return
+
+                AppManager.selected_shape.move_right()
+                return
 
     def _on_mouse_move(self, event) -> None:
         """
         Handles mouse move events
         """
-        if Manager.clicked_button:
-            if self.dragging:
+        if self.dragging:
+            # if AppManager.selected_shape:
+            #     AppManager.selected_shape.move(*self.current_coordinates)
+            #     return
+
+            if AppManager.clicked_button:
                 self.current_coordinates = (event.x, event.y)
+                return
 
     def _on_mouse_press(self, event) -> None:
         """
         Handles mouse press events
         """
-        if Manager.clicked_button:
+        if AppManager.clicked_button:
             self.dragging = True
             self.start_coordinates = (event.x, event.y)
             self.current_coordinates = self.start_coordinates
             return
 
-        if self.shapes:
-            for shape in self.shapes:
-                if shape.within_bounds(event.x, event.y):
-                    if Manager.selected_shape:
-                        Manager.selected_shape.selected = False
+        within_any_shape_bounds: bool = False
 
-                        shape.selected = True
-                        Manager.selected_shape = shape
-                        break;
-                    else:
-                        shape.selected = True
-                        Manager.selected_shape = shape
+        if not self.shapes:
+            return
+
+        for shape in self.shapes:
+            if not shape.within_bounds(event.x, event.y):
+                continue
+
+            within_any_shape_bounds = True
+
+            # changes to a different selected shape
+            if AppManager.selected_shape:
+                AppManager.selected_shape.selected = False
+
+                AppManager.selected_shape = shape
+                AppManager.selected_shape.selected = True
+                break
+
+            # first selected shape
+            shape.selected = True
+            AppManager.selected_shape = shape
+
+        # user clicks on the canvas only
+        if not within_any_shape_bounds and AppManager.selected_shape:
+            AppManager.selected_shape.selected = False
+            AppManager.selected_shape = None
 
     def _on_mouse_release(self, event):
         """
         Handles mouse release events
         """
-        if Manager.clicked_button:
+        if AppManager.clicked_button:
             if self.dragging:
                 self.end_coordinates = (event.x, event.y)
                 self.insert_shape(self.start_coordinates, self.end_coordinates)
 
-            self.dragging = False
-            self.start_coordinates = None
-            self.end_coordinates = None
-            return
+        self.dragging = False
+        self.start_coordinates = None
+        self.end_coordinates = None
+        return
 
     def initgl(self) -> None:
         """
@@ -150,7 +224,7 @@ class OpenGLCanvas(pyopengltk.OpenGLFrame):
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
 
-        if self.dragging and Manager.clicked_button:
+        if self.dragging and AppManager.clicked_button:
             glBegin(GL_LINES)
             glVertex2f(*self.start_coordinates)
             glVertex2f(*self.current_coordinates)
@@ -161,17 +235,17 @@ class OpenGLCanvas(pyopengltk.OpenGLFrame):
             Shape.canvas_height = self.winfo_height()
 
             for shape in self.shapes:
-                shape.draw()
+                shape.draw_to_canvas()
 
     def insert_shape(self, start_coordinates, end_coordinates) -> None:
         """
         Inserts a shape into the canvas
         """
-        shape_class_reference = Shape.shapes().get(Manager.clicked_button.shape_name)
-        shape_instance = shape_class_reference(start_coordinates=start_coordinates, end_coordinates=end_coordinates)
+        shape_class_reference = shapes().get(AppManager.clicked_button.image_file_name)
+        shape_instance = shape_class_reference(start_coordinates=list(start_coordinates), end_coordinates=list(end_coordinates))
 
-        Manager.clicked_button.configure(fg_color="transparent")
-        Manager.clicked_button = None
+        AppManager.clicked_button.configure(fg_color="transparent")
+        AppManager.clicked_button = None
 
         self.parent.configure(cursor='arrow')
         self.shapes.append(shape_instance)
