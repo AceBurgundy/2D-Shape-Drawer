ab4fc8e6f4fa88f4214380ab0374ee856f9d03f5
AceBurgundy <samadriansabalo99@gmail.com>
2024-02-24 16:47:38 +0800

    calls update for manager.py on load.

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+from Shapes.auto_manager import update_manager_py
 from Program import App
 
 if __name__ == '__main__':
-    App().mainloop()
+    update: bool = update_manager_py()
+
+    if update:
+        App().mainloop()
