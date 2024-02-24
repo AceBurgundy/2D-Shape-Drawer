2ffd8c39a5dbfe4aa9f790b6b8d8d172fb845d08
AceBurgundy <samadriansabalo99@gmail.com>
2024-02-24 16:52:42 +0800

    Used type checking and added export, import and color picker buttons.

--- Navigation.py
+++ Navigation.py
@@ -4,15 +4,20 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from Buttons.ColorPickerToggle import ColorPickerToggle
+
 if TYPE_CHECKING:
     from Program import App
 
 # start of code
+from Buttons.ImportButton import ImportButton
+from Buttons.ExportButton import ExportButton
+from Buttons.ShapeButton import ShapeButton
 from customtkinter import CTkFrame
-from Shapes.Shape import Shape
-from Button import ImageButton
-from os import listdir, path
 from typing import List
+from constants import *
+
+from Shapes.Manager import names
 
 class Navigation(CTkFrame):
     def __init__(self, parent: App, **kwargs):
@@ -26,31 +31,38 @@ class Navigation(CTkFrame):
         super().__init__(parent, **kwargs)
         self.parent: App = parent
 
-        button_box: CTkFrame = CTkFrame(self)
-        button_box.pack(pady=(5, 0), padx=3)
+        export_button: ExportButton = ExportButton(self, parent.right_content)
+        export_button.pack(fill="both", padx=DEFAULT_PADDING, pady=DEFAULT_PADDING)
 
-        exemptions: List[str] = ["__pycache__", "Shape.py", "__init__.py"]
-        file_names: List[str] = []
+        import_button: ImportButton = ImportButton(self, parent.right_content)
+        import_button.pack(fill="both", pady=BOTTOM_PADDING_ONLY, padx=DEFAULT_PADDING)
 
-        for file_name in listdir('Shapes'):
-            file_path: str = path.join('Shapes', file_name)
-            if file_name not in exemptions and path.isfile(file_path):
-                clean_name: str = file_name.lower().replace('.py', '')
-                file_names.append(clean_name)
+        container: CTkFrame = CTkFrame(self)
+        container.pack(pady=TOP_PADDING_ONLY, padx=3)
 
-        for index, shape_name in enumerate(file_names):
-            button_box.configure(fg_color="transparent")
-            button: ImageButton = ImageButton(button_box, self.parent, shape_name)
+        shape_names: List[str] = names()
+        index: int = 2
 
-            # Creates a new button_box for the next 2 buttons
+        for shape_name in shape_names:
+            container.configure(fg_color="transparent")
+            button: ShapeButton = ShapeButton(container, self.parent, shape_name)
+
+            # Creates a new container for the next 2 buttons
             if (index + 1) % 2 != 0:
                 button.grid(row=0, column=0)
+                index += 1
                 continue
 
             button.grid(row=0, column=1)
-            button_box: CTkFrame = CTkFrame(self)
-            button_box.pack(pady=(5, 0), padx=3)
+            container: CTkFrame = CTkFrame(self)
+            container.pack(pady=TOP_PADDING_ONLY, padx=3)
+            index += 1
+
+        # Inserting the color picker as the last button
+        color_picker: ColorPickerToggle = ColorPickerToggle(container, self.parent)
+        color_picker.grid(row=0, column=0)
+        container.pack(pady=10, padx=10)
 
         # Removes the last button container if it doesn't contain any child
-        if not button_box.winfo_children():
-            button_box.destroy()
+        if not container.winfo_children():
+            container.destroy()
