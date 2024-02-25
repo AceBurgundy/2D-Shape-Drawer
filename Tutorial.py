f3f4b653960b64344f7bcdcc8d3802d6273f5611
AceBurgundy <samadriansabalo99@gmail.com>
2024-02-25 18:37:20 +0800

    Organized code into functions and used type hints.

--- Tutorial.py
+++ Tutorial.py
@@ -2,6 +2,8 @@ from customtkinter import CTk, CTkFrame, CTkButton, CTkLabel
 from custom_types import *
 from constants import *
 
+type PADDING_LIST = List[Tuple[str, Tuple[int, int]]]
+
 class Tutorial(CTk):
     def __init__(self) -> None:
         """
@@ -22,113 +24,96 @@ class Tutorial(CTk):
         self.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
         self.grid_columnconfigure(0, weight=1)
 
-        resize_title = CTkLabel(self, text="Resize")
+        self.resize_section()
+        self.rotate_section()
+        self.movement_section()
+        self.delete_section()
+
+    def resize_section(self) -> None:
+        """
+        Contains controls for resizing
+        """
+        resize_title: CTkLabel = CTkLabel(self, text="Resize")
         resize_title.grid(row=1, column=0, pady=TOP_PADDING_ONLY, sticky="nswe")
 
-        # Increase size
-        div = CTkFrame(self)
+        div: CTkFrame = CTkFrame(self)
         div.grid(row=3, column=0, sticky="ns")
 
-        control = CTkButton(div, height=10, width=10, text='Cntrl')
-        control.grid(row=0, column=0, sticky="nswe", padx=LEFT_PADDING_ONLY, pady=DEFAULT_PADDING)
+        self.resize_buttons(div, 'Increase size', 4)
 
-        plus = CTkLabel(div, text='+', width=5)
-        plus.grid(row=0, column=1, sticky="nswe", padx=DEFAULT_PADDING, pady=DEFAULT_PADDING)
-
-        shift = CTkButton(div, height=10, width=10, text='Shift')
-        shift.grid(row=0, column=2, sticky="nswe", pady=DEFAULT_PADDING)
-
-        equals = CTkLabel(div, text='=', width=5)
-        equals.grid(row=0, column=3, sticky="nswe", padx=DEFAULT_PADDING, pady=DEFAULT_PADDING)
-
-        increase = CTkButton(div, height=10, width=10, text='Increase size')
-        increase.grid(row=0, column=4, sticky="nswe", padx=RIGHT_PADDING_ONLY, pady=DEFAULT_PADDING)
-
-        # Decrease Size
-        div = CTkFrame(self)
+        div: CTkFrame = CTkFrame(self)
         div.grid(row=5, column=0, sticky="ns", pady=TOP_PADDING_ONLY)
 
-        control = CTkButton(div, height=10, width=10, text='Cntrl')
-        control.grid(row=0, column=0, sticky="nswe", padx=LEFT_PADDING_ONLY, pady=DEFAULT_PADDING)
+        self.resize_buttons(div, 'Decrease size', 4)
 
-        plus = CTkLabel(div, height=5, text='+', width=5)
-        plus.grid(row=0, column=1, sticky="nswe", padx=DEFAULT_PADDING, pady=DEFAULT_PADDING)
+    def resize_buttons(self, parent: CTkFrame, text: str, row: int) -> None:
+        """
+        Different button controls for resizing
+        """
+        buttons: PADDING_LIST = [
+            ('Cntrl', LEFT_PADDING_ONLY), ('+', DEFAULT_PADDING), ('Shift', 0), ('=', DEFAULT_PADDING), (text, RIGHT_PADDING_ONLY)
+        ]
 
-        shift = CTkButton(div, height=10, width=10, text='Shift')
-        shift.grid(row=0, column=2, sticky="nswe", pady=DEFAULT_PADDING)
+        for index, (button_text, padx) in enumerate(buttons):
+            control_button = CTkButton(parent, height=10, width=10, text=button_text)
+            control_button.grid(row=0, column=index, sticky="nswe", padx=padx, pady=DEFAULT_PADDING)
 
-        equals = CTkLabel(div, height=5, text='=', width=5)
-        equals.grid(row=0, column=3, sticky="nswe", padx=DEFAULT_PADDING, pady=DEFAULT_PADDING)
-
-        decrease = CTkButton(div, height=10, width=10, text='Decrease size')
-        decrease.grid(row=0, column=4, sticky="nswe", padx=RIGHT_PADDING_ONLY, pady=DEFAULT_PADDING)
-
-        # Rotate counter
-        rotate_title = CTkLabel(self, text="Rotate")
+    def rotate_section(self) -> None:
+        """
+        Contains controls for rotation
+        """
+        rotate_title: CTkLabel = CTkLabel(self, text="Rotate")
         rotate_title.grid(row=7, column=0, sticky="nswe", pady=TOP_PADDING_ONLY)
 
-        div = CTkFrame(self)
+        div: CTkFrame = CTkFrame(self)
         div.grid(row=8, column=0, sticky="ns")
 
-        control = CTkButton(div, height=10, width=10, text='Cntrl')
-        control.grid(row=0, column=0, sticky="nswe", padx=LEFT_PADDING_ONLY, pady=DEFAULT_PADDING)
+        self.rotate_buttons(div, 'Left Arrow')
 
-        plus = CTkLabel(div, height=5, text='+', width=5)
-        plus.grid(row=0, column=1, sticky="nswe", padx=DEFAULT_PADDING, pady=DEFAULT_PADDING)
-
-        shift = CTkButton(div, height=10, width=10, text='Shift')
-        shift.grid(row=0, column=2, sticky="nswe", pady=DEFAULT_PADDING)
-
-        equals = CTkLabel(div, height=5, text='=', width=5)
-        equals.grid(row=0, column=3, sticky="nswe", padx=DEFAULT_PADDING, pady=DEFAULT_PADDING)
-
-        left_arrow = CTkButton(div, height=10, width=10, text='Left Arrow')
-        left_arrow.grid(row=0, column=4, sticky="nswe", padx=RIGHT_PADDING_ONLY, pady=DEFAULT_PADDING)
-
-        # Rotate
-        div = CTkFrame(self)
+        div: CTkFrame = CTkFrame(self)
         div.grid(row=9, column=0, sticky="ns", pady=TOP_PADDING_ONLY)
 
-        control = CTkButton(div, height=10, width=10, text='Cntrl')
-        control.grid(row=0, column=0, sticky="nswe", padx=LEFT_PADDING_ONLY, pady=DEFAULT_PADDING)
+        self.rotate_buttons(div, 'Right Arrow')
 
-        plus = CTkLabel(div, height=10, text='+', width=5)
-        plus.grid(row=0, column=1, sticky="nswe", padx=DEFAULT_PADDING, pady=DEFAULT_PADDING)
+    def rotate_buttons(self, parent: CTkFrame, text: str) -> None:
+        """
+        Different button controls for rotation
+        """
+        buttons: PADDING_LIST = [
+            ('Cntrl', LEFT_PADDING_ONLY), ('+', DEFAULT_PADDING), ('Shift', 0), ('=', DEFAULT_PADDING), (text, RIGHT_PADDING_ONLY)
+        ]
 
-        shift = CTkButton(div, height=10, width=10, text='Shift')
-        shift.grid(row=0, column=2, sticky="nswe", pady=DEFAULT_PADDING)
+        for index, (button_text, padx) in enumerate(buttons):
+            control_button = CTkButton(parent, height=10, width=10, text=button_text)
+            control_button.grid(row=0, column=index, sticky="nswe", padx=padx, pady=DEFAULT_PADDING)
 
-        equals = CTkLabel(div, height=1, text='=', width=5)
-        equals.grid(row=0, column=3, sticky="nswe", padx=DEFAULT_PADDING, pady=DEFAULT_PADDING)
-
-        right_arrow = CTkButton(div, height=10, width=10, text='Right Arrow')
-        right_arrow.grid(row=0, column=4, sticky="nswe", padx=RIGHT_PADDING_ONLY, pady=DEFAULT_PADDING)
-
-        # Movement
-        arrow_titles = CTkLabel(self, text="Movement")
+    def movement_section(self) -> None:
+        """
+        Contains different button for shape movement
+        """
+        arrow_titles: CTkLabel = CTkLabel(self, text="Movement")
         arrow_titles.grid(row=10, column=0, sticky="nswe", pady=TOP_PADDING_ONLY)
 
-        div = CTkFrame(self)
+        div: CTkFrame = CTkFrame(self)
         div.grid(row=11, column=0, sticky="ns")
 
-        left_arrow = CTkButton(div, height=10, width=10, text='<')
-        left_arrow.grid(row=0, column=0, sticky="nswe", padx=DEFAULT_PADDING, pady=DEFAULT_PADDING)
+        buttons: PADDING_LIST = [
+            ('Left Arrow', DEFAULT_PADDING), ('>', RIGHT_PADDING_ONLY), ('^', DEFAULT_PADDING), ('v', DEFAULT_PADDING)
+        ]
 
-        right_arrow = CTkButton(div, height=10, width=10, text='>')
-        right_arrow.grid(row=0, column=2, sticky="nswe", padx=RIGHT_PADDING_ONLY, pady=DEFAULT_PADDING)
+        for index, (button_text, padx) in enumerate(buttons):
+            control_button = CTkButton(div, height=10, width=10, text=button_text)
+            control_button.grid(row=0, column=index, sticky="nswe", padx=padx, pady=DEFAULT_PADDING)
 
-        top_arrow = CTkButton(div, height=10, text='^', width=5)
-        top_arrow.grid(row=0, column=3, sticky="nswe", pady=DEFAULT_PADDING)
-
-        bottom_arrow = CTkButton(div, height=10, width=10, text='v')
-        bottom_arrow.grid(row=0, column=4, sticky="nswe", padx=DEFAULT_PADDING, pady=DEFAULT_PADDING)
-
-        # Delete
-        delete_title = CTkLabel(self, text="Movement")
+    def delete_section(self) -> None:
+        """
+        Contains button for delete
+        """
+        delete_title: CTkLabel = CTkLabel(self, text="Delete")
         delete_title.grid(row=12, column=0, sticky="nswe", pady=TOP_PADDING_ONLY)
 
-        div = CTkFrame(self)
+        div: CTkFrame = CTkFrame(self)
         div.grid(row=13, column=0, sticky="ns")
 
-        delete = CTkButton(div, height=10, width=10, text='Delete')
+        delete: CTkButton = CTkButton(div, height=10, width=10, text='Delete')
         delete.grid(row=0, column=0, sticky="nswe", padx=DEFAULT_PADDING, pady=DEFAULT_PADDING)
