8e7dedae27e0399f17704a3e204569f8dcc34f87
AceBurgundy <samadriansabalo99@gmail.com>
2024-02-25 16:56:40 +0800

    moved import later after updating Manager.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from Shapes.auto_manager import update_manager_py
-from Program import App
 
 if __name__ == '__main__':
     update: bool = update_manager_py()
+
+    from Program import App
 
     if update:
         App().mainloop()
